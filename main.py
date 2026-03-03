0cf956117f78eb92402f87b87244871273f8eee0
Alex Mineeva <amineeva@olin.edu>
2026-03-03 14:13:52 -0500

    Add 3/3 goals to main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 """
 Contains main script to run machine learning algorithms on the data.
 """
+
+
+# Goals:
+# - load in data (done!)
+# - create basic plots 
+# - Read Asteroid Paper
+# - Determine what machine learning methods I want to try
+# - Determine which asteroid families I will attempt to identify and why
+# - Research clustering method 1
