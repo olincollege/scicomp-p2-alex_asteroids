1584bdf996243554413bb790627029df3c2525e2
Alex Mineeva <amineeva@olin.edu>
2026-03-04 00:30:03 -0500

    Update to-do list

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ Contains main script to run machine learning algorithms on the data.
 
 # Goals:
 # - load in data (done!)
-# - create basic plots 
+# - create basic plots (done)
 # - Read Asteroid Paper
-# - Determine what machine learning methods I want to try
+# - Determine what machine learning methods I want to try (done) -> K-means and Heirarchical clustering (more notes on this in my notes doc)
 # - Determine which asteroid families I will attempt to identify and why
 # - Research clustering method 1
