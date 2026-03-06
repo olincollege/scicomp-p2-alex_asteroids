b5c1a9f80c6d2ec6ddaa1c9ae9e38f6c93362ae3
Alex Mineeva <amineeva@olin.edu>
2026-03-06 11:40:44 -0500

    Modify to convert validation dataset txt > csv (all_tro.members)

--- txt_csv_converter.py
+++ txt_csv_converter.py
@@ -5,22 +5,38 @@ Converts .txt dataset into usable .csv file that will run with the machine learn
 import pandas as pd
 
 base_path = "/mnt/c/Users/amineeva/OneDrive - Olin College of Engineering/2025-2026/Semester 2/Scientific Computing/Project 2 - Asteroids/"
-asteroid_data_download_path = base_path + "Synthetic_Proper_Elements_Hirayama_full_dataset.txt"
-asteroid_data_output_path = base_path + "Synthetic_Proper_Elements_Hirayama_full_dataset.csv"
+# asteroid_data_download_path = base_path + "Synthetic_Proper_Elements_Hirayama_full_dataset.txt"
+# asteroid_data_output_path = base_path + "Synthetic_Proper_Elements_Hirayama_full_dataset.csv"
+
+asteroid_data_download_path = base_path + "all_tro.members.txt"
+asteroid_data_output_path = base_path + "all_tro.members.csv"
 
 # Column names
+# columns = [
+#     "Name",
+#     "mag",
+#     "a_AU",
+#     "e",
+#     "sin_I",
+#     "n_deg_per_yr",
+#     "g_arcsec_per_yr",
+#     "s_arcsec_per_yr",
+#     "LCEx1E6",
+#     "My"
+# ]
 columns = [
-    "Name",
-    "mag",
-    "a_AU",
-    "e",
-    "sin_I",
-    "n_deg_per_yr",
-    "g_arcsec_per_yr",
-    "s_arcsec_per_yr",
-    "LCEx1E6",
-    "My"
-]
+  "ast_name",
+  "Hmag",
+  "status",
+  "family1",
+  "dv_fam1",
+  "near1",
+  "family2",
+  "dv_fam2",
+  "near2",
+  "rescod"
+  ]
+
 
 # Convert downloaded .txt file stored locally to .csv file
 df = pd.read_csv(
