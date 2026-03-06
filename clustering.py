4973608d2cb665e8696e4d868996afdc9d229e34
Alex Mineeva <amineeva@olin.edu>
2026-03-06 11:41:15 -0500

    Create file to hold clustering algorithms

---
+++ clustering.py
@@ -0,0 +1,15 @@
+"""
+Contains code for clustering algorithms
+"""
+import pandas as pd
+import matplotlib.pyplot as mp
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import cluster
+from sklearn.preprocessing import StandardScaler, normalize
+
+# Method 1: Aglomerative Clustering
+# should have n_clusters and linkage as parameters
+
+
+# Method 2:
