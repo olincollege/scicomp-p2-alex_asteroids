58f1e73aa67043277d5b0b70e0006039d325d5ba
Alex Mineeva <amineeva@olin.edu>
2026-03-12 10:03:18 -0400

    Write plan for benchmark validation

---
+++ benchmark_validation.py
@@ -0,0 +1,17 @@
+"""
+Compares clustered data to labeled data, gives a correctness score.
+The benchmark for this project is 95% correctness.
+"""
+
+##### Imports #####
+
+
+# input the labeled dataset
+
+# input the cluster dataset
+
+# match the clusters on the labeled dataset to the input cluster dataset
+
+# output table with the cluster numbers, family numbers, and the correctness, sorted from most correct to lowest
+
+# plot most correct clusters, color correct, grey out incorrect?
