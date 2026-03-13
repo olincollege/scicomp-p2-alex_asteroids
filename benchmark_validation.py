d93177110412005eb7cd974166270d1985c7898e
Alex Mineeva <amineeva@olin.edu>
2026-03-12 21:19:43 -0400

    Add output csv with purity and completeness metrics per family

--- benchmark_validation.py
+++ benchmark_validation.py
@@ -5,7 +5,7 @@ Metrics:
     Purity: correctly clustered asteroids / total clustered asteroids from labaled dataset
         -> are all of the asteroids in the cluster determined to be a particular family actually part of that family?
         -> How pure are my clusters? (ignoring unclustered (-1) asteroids)
-    Accuracy: correctly clustered asteroids / all labeled asteroids
+    Completeness: correctly clustered asteroids / all labeled asteroids
         -> how many of the labeled asteroids made it into the cluster?
         -> How many labeled asteroids did the cluster actually get? (unclustered asteroids count as missing)
 
@@ -24,7 +24,7 @@ df_labeled = pd.read_csv(validation_data_path)
 
 # cluster dataset
 base_path = "/mnt/c/Users/amineeva/OneDrive - Olin College of Engineering/2025-2026/Semester 2/Scientific Computing/Project 2 - Asteroids/"
-clustered_data_path = base_path + "asteroid_clusters_full_r0012.csv"
+clustered_data_path = base_path + "ball_tree_asteroid_clusters_full_r0018.csv"
 df_clustered = pd.read_csv(clustered_data_path)
 
 
@@ -124,4 +124,24 @@ output_table = df_merged_completeness[[
     "Name", "cluster_id", "determined_family", "family1", "correct"
 ]].sort_values(by="correct", ascending=False)
 
-output_table.to_excel(base_path + "cluster_validation_correctness.csv", index=False)
+output_table.to_csv(base_path + "cluster_validation_correctness.csv", index=False)
+
+# completeness per family #
+family_completeness = df_merged_completeness.groupby("family1").agg(total_family_asteroids=("Name", "count"), correctly_clustered=("correct", "sum")).reset_index()
+family_completeness["completeness"] = (family_completeness["correctly_clustered"] / family_completeness["total_family_asteroids"])
+
+# purity per family #
+cluster_purity = df_merged_purity.groupby("cluster_id").agg(cluster_size=("Name", "count"), correct_members=("correct", "sum"), determined_family=("determined_family", "first")).reset_index()
+cluster_purity["purity"] = (cluster_purity["correct_members"] / cluster_purity["cluster_size"])
+family_purity = cluster_purity.groupby("determined_family")["purity"].mean().reset_index()
+family_purity.rename(columns={"determined_family": "family1"}, inplace=True)
+
+# combine completeness and purity #
+family_validation = family_completeness.merge(family_purity, on="family1", how="left")
+family_validation = family_validation.sort_values("completeness", ascending=False)
+
+# export #
+family_validation.to_csv(base_path + "family_validation_summary.csv", index=False)
+
+print("Family validation summary saved.")
+print(family_validation.head())
